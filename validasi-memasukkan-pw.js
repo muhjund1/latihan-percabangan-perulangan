cb5b1b04446ac4a94faaff2ccb043480a1cf1a42
Muhammad Jundi Al-Aqsha <muhammadjunnndi@gmail.com>
2026-09-22 13:19:36 +0700

    latihan 1

---
+++ validasi-memasukkan-pw.js
@@ -0,0 +1,24 @@
+const readline = require('readline');
+    
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    });
+    
+    function askQuestion(query) {
+      return new Promise((resolve) => rl.question(query, resolve));
+    }
+    
+    (async () => {
+      const correctPassword = "123456"; // Password yg bener
+    
+      const password = await askQuestion('Masukkan password Anda: ');
+    
+      if (password === correctPassword) {
+        console.log("Password benar. Akses diterima.");
+      } else {
+        console.log("Password salah. Akses ditolak.");
+      }
+    
+      rl.close();
+    })();
